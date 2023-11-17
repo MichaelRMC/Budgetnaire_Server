a646c5b275dba282356fb732d9ceed1d6f6d55d4
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-11-16 22:25:42 -0500

    Update App.jsx

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const transactions = require('./controllers/transactionController.js')
 
 const app = express();
 
-app.use(express.json())
 app.use(cors());                        
+app.use(express.json())
 app.use("/transactions", transactions )
 
 
