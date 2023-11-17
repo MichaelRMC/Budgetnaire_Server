67205d48cfe61bec968cd176363c45c91aa7852a
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-11-17 06:36:13 -0500

    Update models

--- models/transaction.js
+++ models/transaction.js
@@ -1,6 +1,5 @@
 module.exports = [
 	{
-		id: "01HD6B0N5MNS9662BEXQ5W761K",
 		item_name: "Payment",
 		amount: "$24.83",
 		date: "01/16/2023",
@@ -8,7 +7,6 @@ module.exports = [
 		category: "Entertainment",
 	},
 	{
-		id: "01HD6B0N5S0J654735TQ72A1XT",
 		item_name: "Deposit",
 		amount: "$500.00",
 		date: "03/09/2023",
@@ -16,7 +14,6 @@ module.exports = [
 		category: "Savings",
 	},
 	{
-		id: "01HD6B0N5TRX7251NNBJAB51Y0",
 		item_name: "Income",
 		amount: "$2,788",
 		date: "02/25/2023",
@@ -24,7 +21,7 @@ module.exports = [
 		category: "Income",
 	},
 	{
-		id: "01HD6B0N5T5SX9DW2VAEJVW8R9",
+
 		item_name: "Payment",
 		amount: "$29.31",
 		date: "10/05/2022",
@@ -32,7 +29,6 @@ module.exports = [
 		category: "Pets",
 	},
 	{
-		id: "01HD6B0N5VN3F0NK3P4WF9QJXQ",
 		item_name: "Withdrawal",
 		amount: "100.00",
 		date: "05/21/2023",
@@ -40,7 +36,6 @@ module.exports = [
 		category: "Personal",
 	},
 	{
-		id: "01HD6B0N5WYSJZFKR10DZYBSRJ",
 		item_name: "Income",
 		amount: "$2,788",
 		date: "02/26/2023",
@@ -48,15 +43,13 @@ module.exports = [
 		category: "Income",
 	},
 	{
-		id: "01HD6B0N5X8FE4S2AW3Y8Z8YRE",
 		item_name: "Payment",
 		amount: "$2,000",
 		date: "05/11/2023",
 		business: "Landlord",
 		category: "Housing",
 	},
-	{
-		id: "01HD6B0N5YA0GNZBCMZZ4TPG1M",
+	{	
 		item_name: "Payment",
 		amount: "$132.00",
 		date: "01/14/2023",
@@ -64,7 +57,6 @@ module.exports = [
 		category: "Transportation",
 	},
 	{
-		id: "01HD6B0N5YS08TZJGY43B3H12W",
 		item_name: "Payment",
 		amount: "$21.59",
 		date: "04/09/2023",
@@ -72,7 +64,6 @@ module.exports = [
 		category: "Entertainment",
 	},
 	{
-		id: "01HD6B0N5Z77W28JS7AR5BGH7Z",
 		item_name: "Xbox Game Pass Ultimate",
 		amount: "$18.35",
 		date: "05/12/2023",
@@ -80,7 +71,6 @@ module.exports = [
 		category: "Entertainment",
 	},
 	{
-		id: "01HD6B0N61WQEY9N52HDTWYXC7",
 		item_name: "Payment",
 		amount: "$563.00",
 		date: "02/17/2023",
@@ -88,7 +78,6 @@ module.exports = [
 		category: "Medical",
 	},
 	{
-		id: "01HD6B0N62NGGS8J7KBNZA6K43",
 		item_name: "Payment",
 		amount: "100.00",
 		date: "04/25/2023",
